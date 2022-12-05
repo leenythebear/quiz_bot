9f802ba57f469417cbeed817b672023e222c5783
leeny_the_bear <leeny@inbox.ru>
2022-12-05 19:12:45 +0600

    rename function in the handler

--- tg_bot.py
+++ tg_bot.py
@@ -61,7 +61,7 @@ def main():
     dp.add_handler(CommandHandler("help", help))
 
     # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
+    dp.add_handler(MessageHandler(Filters.text, start_game))
 
     # log all errors
     dp.add_error_handler(error)
