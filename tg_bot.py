472f085f5795815d8c6361921fca26f034543f44
leeny_the_bear <leeny@inbox.ru>
2022-12-12 15:27:06 +0600

    del comments

--- tg_bot.py
+++ tg_bot.py
@@ -75,10 +75,8 @@ def main():
 
     updater = Updater(telegram_token)
 
-    # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
-    # on different commands - answer in Telegram
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler("start", start)],
         states={
@@ -93,18 +91,10 @@ def main():
                    ]
     )
 
-    # on noncommand i.e message - echo the message on Telegram
-
     dp.add_handler(conv_handler)
-    # log all errors
-    dp.add_error_handler(error)
 
-    # Start the Bot
     updater.start_polling()
 
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
 
 
