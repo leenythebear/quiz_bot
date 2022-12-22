7656fa1b8802faa1f9c0814f52cd400dbc1d8b40
leeny_the_bear <leeny@inbox.ru>
2022-12-22 22:05:03 +0600

    add vk_token and path to the questions file to the settings

--- settings.py
+++ settings.py
@@ -1,5 +1,7 @@
 import os
+
 from dotenv import load_dotenv
+
 
 load_dotenv()
 
@@ -8,3 +10,7 @@ redis_port = os.environ['DB_PORT']
 redis_password = os.environ['DB_PASSWORD']
 
 telegram_token = os.environ['TG_TOKEN']
+vk_token = os.environ['VK_TOKEN']
+
+questions_path = os.environ['QUESTION_PATH']
+
